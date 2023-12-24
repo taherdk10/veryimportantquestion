a7700ed019dc2fe2ba7642082498c108458eb2bd
taherdk10 <127022487+taherdk10@users.noreply.github.com>
2023-12-24 20:51:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// Get buttons
-const noButton = document.getElementById('noBtn');
-const yesButton = document.getElementById('yesBtn');
-
-// Function to move the 'No' button to a random position
-function moveButton() {
-  const x = Math.random() * (window.innerWidth - 100);
-  const y = Math.random() * (window.innerHeight - 100);
-  noButton.style.position = 'absolute';
-  noButton.style.left = `${x}px`;
-  noButton.style.top = `${y}px`;
-  noButton.disabled = false;
+function movebtn(){
+  btn = document.getElementById('noBtn');
+  moveButton(btn);
 }
-
-// Disable 'No' button and move it on click
-noButton.addEventListener('click', () => {
-  noButton.style.display = 'none'; // Hide the button
-  moveButton(); // Move the button to a random position
-  setTimeout(() => {
-    noButton.style.display = 'block'; // Show the button after a short delay
-  }, 100); // Adjust the delay as needed
-});
-
-// Redirect to the second page on 'Yes' button click
-yesButton.addEventListener('click', () => {
-  window.location.href = 'thankyou.html';
-});
+function moveButton(btn) {
+  x = Math.random() * (window.innerWidth - 100);
+  y = Math.random() * (window.innerHeight - 100);
+  btn.style.position = 'absolute';
+  btn.style.left = `${x}px`;
+  btn.style.top = `${y}px`;
+  btn.disabled = false;
+}
